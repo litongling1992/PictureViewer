88425d43885bbab3fbbb1c5909055c5d73dee788
li_tl <763351670@qq.com>
2019-01-29 11:41:20 +0800

    first commot

---
+++ PictureViewer/MainWindow.xaml.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PictureViewer
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private bool mouseDown;
+        private Point mouseXY;
+        private double min = 0.1, max = 3.0;//最小/最大放大倍数
+
+
+        private void Domousemove(ContentControl img, MouseEventArgs e)
+        {
+            if (e.LeftButton != MouseButtonState.Pressed)
+            {
+                return;
+            }
+            var group = IMG.FindResource("TfGroup") as TransformGroup;
+            var transform = group.Children[1] as TranslateTransform;
+            var position = e.GetPosition(img);
+            transform.X -= mouseXY.X - position.X;
+            transform.Y -= mouseXY.Y - position.Y;
+            mouseXY = position;
+        }
+
+
+        private void DowheelZoom(TransformGroup group, Point point, double delta)
+        {
+            var pointToContent = group.Inverse.Transform(point);
+            var transform = group.Children[0] as ScaleTransform;
+            if (transform.ScaleX + delta < min) return;
+            if (transform.ScaleX + delta > max) return;
+            transform.ScaleX += delta;
+            transform.ScaleY += delta;
+            var transform1 = group.Children[1] as TranslateTransform;
+            transform1.X = -1 * ((pointToContent.X * transform.ScaleX) - point.X);
+            transform1.Y = -1 * ((pointToContent.Y * transform.ScaleY) - point.Y);
+        }
+
+
+        private void ContentControl_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var img = sender as ContentControl;
+            if (img == null)
+            {
+                return;
+            }
+            img.CaptureMouse();
+            mouseDown = true;
+            mouseXY = e.GetPosition(img);
+        }
+
+        private void ContentControl_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            var img = sender as ContentControl;
+            if (img == null)
+            {
+                return;
+            }
+            img.ReleaseMouseCapture();
+            mouseDown = false;
+        }
+
+        private void ContentControl_MouseMove(object sender, MouseEventArgs e)
+        {
+            var img = sender as ContentControl;
+            if (img == null)
+            {
+                return;
+            }
+            if (mouseDown)
+            {
+                Domousemove(img, e);
+            }
+        }
+
+        private void ContentControl_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            var img = sender as ContentControl;
+            if (img == null)
+            {
+                return;
+            }
+            var point = e.GetPosition(img);
+            var group = IMG.FindResource("TfGroup") as TransformGroup;
+            var delta = e.Delta * 0.001;
+            DowheelZoom(group, point, delta);
+        }
+
+        private void OpenImg_Click(object sender, RoutedEventArgs e)
+        {
+            // 在WPF中， OpenFileDialog位于Microsoft.Win32名称空间            
+            Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.Filter = "Files (*.png)|*.png|Files(*.jpg)|*.jpg";
+            if (dialog.ShowDialog() == true)
+            {
+                //MessageBox.Show(dialog.FileName);
+                this.IMG.Source = new BitmapImage(new Uri(dialog.FileName));
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            setViewSize();
+        }
+        private void setViewSize()
+        {
+            mainScrollv.Width = this.ActualWidth;
+            mainScrollv.Height = this.ActualHeight - 50;
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            setViewSize();
+        }
+
+        private void txtMinSize_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.min = double.Parse(txtMinSize.Text);
+        }
+
+        private void txtMaxSize_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.max = double.Parse(txtMaxSize.Text);
+        }
+
+        private void Window_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effects = DragDropEffects.Link;//WinForm中为e.Effect = DragDropEffects.Link
+            else e.Effects = DragDropEffects.None;//WinFrom中为e.Effect = DragDropEffects.None
+
+        }
+
+        private void Window_Drop(object sender, DragEventArgs e)
+        {
+            string filename = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+            this.IMG.Source = new BitmapImage(new Uri(filename));
+        }
+
+
+    }
+}
